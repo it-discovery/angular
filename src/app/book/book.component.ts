e35c3bb29dc15b651de51e9cc5f61dff5a7cb5ab
Sergiy <sergey.morenets@gmail.com>
2024-04-17 15:24:43 +0300

    Task 5

--- src/app/book/book.component.ts
+++ src/app/book/book.component.ts
@@ -1,4 +1,4 @@
-import {Component} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {Book} from "../book";
 
 @Component({
@@ -6,13 +6,22 @@ import {Book} from "../book";
   templateUrl: './book.component.html',
   styleUrl: './book.component.css'
 })
-export class BookComponent {
+export class BookComponent implements OnInit {
   book: Book;
+
+  symbolCount: number | undefined;
 
   constructor() {
     this.book = {
       title: 'Angular 17',
       year: 2024, author: 'N/A', pages: 500, description: 'Development of Angular applications'
     };
+  }
+
+  onClick(value: string | number | undefined): void {
+    this.symbolCount = value?.toString().length;
+  }
+
+  ngOnInit(): void {
   }
 }
