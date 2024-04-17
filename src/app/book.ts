06f76d2b09ae19d5ab4ad31c420eb88d954d8a29
Sergiy <sergey.morenets@gmail.com>
2024-04-17 14:49:47 +0300

    Task 4.3. Combine properties declaration and initialization in the Book class

--- src/app/book.ts
+++ src/app/book.ts
@@ -1,9 +1,13 @@
 type StringOrEmpty = string | undefined;
 
 export class Book {
-  title: StringOrEmpty;
-  year: number | undefined;
-  author: StringOrEmpty;
+  // title: StringOrEmpty;
+  // year: number | undefined;
+  // author: StringOrEmpty;
   pages?: number;
   description?: string;
+
+  constructor(public title: string, public year: number,
+              public author: string) {
+  }
 }
